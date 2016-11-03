9cb9f73d8763f599afdd22c899cc088ff98d28c4
KailasPrajapati <kailas.prajapati@softwebsolutions.com>
2016-11-03 23:32:31 +0530

    project

---
+++ src/main/java/com/sample/HelloWorldConfig.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class HelloWorldConfig {
+
+	/*@Bean 
+	public HelloWorld helloWorld(){
+		return new HelloWorld();
+	}*/
+	
+}
